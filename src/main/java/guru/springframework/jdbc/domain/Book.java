303e759ad0d165e1f6e33e0670ec7df40ed87c18
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-09-13 21:50:07 -0400

    Commit of Book and Author files and execution of the flyway init_database.

---
+++ src/main/java/guru/springframework/jdbc/domain/Book.java
@@ -0,0 +1,47 @@
+package guru.springframework.jdbc.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String isbn;
+
+    private String publisher;
+
+    private String title;
+
+    private Long authorId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ src/main/java/guru/springframework/jdbc/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.jdbc.repositories;
+
+import guru.springframework.jdbc.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
